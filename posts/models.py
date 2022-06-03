63353010e3eb34cf2778495e975d495406dbb313
leopoldo-1 <leopoldo.miranda18@tectijuana.edu.mx>
2022-06-02 17:31:52 -0700

    first commit

---
+++ posts/models.py
@@ -0,0 +1,16 @@
+#models = modules
+from django.db import models
+from django.urls import reverse
+
+# Create your models here.
+class Post(models.Model):
+  #class attributes/ columnes db-table
+  title = models.CharField(max_length=255) #title extends form charfield
+  body = models.TextField()
+  created_on = models.DateTimeField(auto_now_add=True)
+
+  def __str__(self):
+    return self.title
+
+  def get_absolute_ur(self):
+    return reverse('post_detail', args=[self.id])
