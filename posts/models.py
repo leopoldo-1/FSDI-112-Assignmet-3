2d49662d9aebeacae8e2cb798d16a07dd1886a61
leopoldo-1 <leopoldo.miranda18@tectijuana.edu.mx>
2022-06-06 15:53:35 -0700

    templates updated

--- posts/models.py
+++ posts/models.py
@@ -12,5 +12,5 @@ class Post(models.Model):
   def __str__(self):
     return self.title
 
-  def get_absolute_ur(self):
+  def get_absolute_url(self):
     return reverse('post_detail', args=[self.id])
